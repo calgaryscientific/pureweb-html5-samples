dcfda1dec6f0d48a0faec130c98e2b3192786fef
Matthew Stephure <matt.stephure@calgaryscientific.com>
2014-04-16 11:42:23 -0600

    PWEB-4165 Preserve attributes in sample deploy scripts.

--- prod/Asteroids/AsteroidsClientHTML5/deploy.sh
+++ prod/Asteroids/AsteroidsClientHTML5/deploy.sh
@@ -5,10 +5,10 @@
 echo Deploying to PureWeb server: PUREWEB_HOME=${PUREWEB_HOME}
 SAVE_DIR=`pwd`
 cd `dirname $0`
-cp *.html ${PUREWEB_HOME}/webapp
-cp *.js ${PUREWEB_HOME}/webapp
-cp -r ./css ${PUREWEB_HOME}/webapp
-cp -r ./img ${PUREWEB_HOME}/webapp
+cp -a *.html ${PUREWEB_HOME}/webapp
+cp -a *.js ${PUREWEB_HOME}/webapp
+cp -ar ./css ${PUREWEB_HOME}/webapp
+cp -ar ./img ${PUREWEB_HOME}/webapp
 mkdir ${PUREWEB_HOME}/webapp/lib/pureweb/
-cp ${PUREWEB_LIBS}/HTML5/pureweb.min.js ${PUREWEB_HOME}/webapp/lib/pureweb/
+cp -a ${PUREWEB_LIBS}/HTML5/pureweb.min.js ${PUREWEB_HOME}/webapp/lib/pureweb/
 cd ${SAVE_DIR}
