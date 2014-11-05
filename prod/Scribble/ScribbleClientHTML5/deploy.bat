d52071d75f57d9c36a46a097608fdd850befa891
Matthew Stephure <matt.stephure@calgaryscientific.com>
2014-11-05 16:30:04 -0700

    PWEB-4693 Bring in HTML5 samples

--- prod/Scribble/ScribbleClientHTML5/deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-@echo Deploying sample to PureWeb server: PUREWEB_HOME=%PUREWEB_HOME%
-pushd %~dp0
-xcopy /E /I /Y /D *.* %PUREWEB_HOME%\webapp
-xcopy /E /I /Y /D  %PUREWEB_LIBS%\HTML5\*.* %PUREWEB_HOME%\webapp\lib\pureweb\
-popd
